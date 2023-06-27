1ae8fa56d8a556ecf3ea478834bfbddc2cb904b1
emmanuelmoon <emmenual123@gmail.com>
2023-06-27 21:13:53 +0500

    Added AirBnB ESLint config.

--- bloglist/app.js
+++ bloglist/app.js
@@ -1,23 +1,25 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blog')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
 
-mongoose.set('strictQuery', false)
-app.use('/api/blogs', blogsRouter)
+const app = express();
+
+const blogsRouter = require('./controllers/blog');
+const logger = require('./utils/logger');
+
+mongoose.set('strictQuery', false);
+app.use('/api/blogs', blogsRouter);
 
 mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-module.exports = app
+module.exports = app;
