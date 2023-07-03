fd77eaa2cc7dbb1007476e8f0a2448d83c1ab07a
emmanuelmoon <emmenual123@gmail.com>
2023-07-03 19:53:48 +0500

    Completed 4.6

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const _ = require('lodash');
+
 const dummy = () => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
@@ -14,8 +17,28 @@ const favoriteBlog = (blogs) => {
   };
 };
 
+const mostBlogs = (blogs) => {
+  const obj = {};
+
+  blogs.forEach((element) => {
+    if (obj[element.author] === undefined) {
+      obj[element.author] = 1;
+    } else {
+      obj[element.author] += 1;
+    }
+  });
+
+  const maxKey = _.max(Object.keys(obj), (o) => obj[o]);
+
+  return {
+    author: maxKey,
+    blogs: obj[maxKey],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
