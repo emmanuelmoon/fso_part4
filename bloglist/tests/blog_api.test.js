4ddb802e1015cb6a01c0883604cfdca532da532d
emmanuelmoon <emmenual123@gmail.com>
2023-07-06 21:15:37 +0500

    Completed 4.11

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -61,6 +61,37 @@ test('blog post is saved correctly', async () => {
   );
 });
 
+test('likes default to zero when the field is missing', async () => {
+  const newBlog = {
+    title: 'npm audit: Broken by Design',
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/npm-audit-broken-by-design/',
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const blogs = blogsAtEnd.map((blog) => ({
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+  }));
+
+  expect(blogs).toContainEqual({
+    title: 'npm audit: Broken by Design',
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/npm-audit-broken-by-design/',
+    likes: 0,
+  });
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 }, 100000);
